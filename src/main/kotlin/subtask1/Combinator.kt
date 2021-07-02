896e139d44bc1994805c68919f5ec88ea45e7b26
Ziban89 <ziban89@tut.by>
2021-07-02 15:38:05 +0300

    feat: solve task

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -1,9 +1,15 @@
 package subtask1
 
 class Combinator {
-
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        for(i in 1..array[1]){
+            if(array[0] * i.factorial() * (array[1] - i).factorial() <= array[1].factorial())
+                return i
+        }
+        return null
     }
+}
+private fun Int.factorial(): Int {
+    if(this == 1 || this == 0) return 1
+    return (this-1).factorial() * this
 }
--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,28 @@
 package subtask2
 
+
+import kotlin.math.*
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        var arr: Array<Int>?
+        for (i in number-1 downTo 1) {
+            arr = (number * number - i * i).getMaxMember()
+            if(arr != null) return arr.plus(i)
+        }
+        return null
+
+    }
+
+    private fun Int.getMaxMember(): Array<Int>? {
+        val maxMemb = (sqrt(this.toDouble())).toInt()
+        var arr: Array<Int>?
+        for (i in maxMemb downTo 1) {
+            if(this - i * i ==0) return Array(1){i}
+            arr = (this - i * i).getMaxMember()
+            if ( arr?.sumBy { it * it } == this - i * i && !arr.contains(i)) return arr.plus(i)
+        }
+        return  null
     }
 }
